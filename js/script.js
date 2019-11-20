1a33075e9b7ad844eb19034c2e7240da030464de
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2019-11-20 12:15:43 -0600

    working on clicking grid item to open new file

--- js/script.js
+++ js/script.js
@@ -1,12 +1,11 @@
 const gridContainer = document.getElementById('gridContainer')
 const grid = document.getElementsByClassName('grid')
 
-for (let i = 0; i < grid.length; i++) {
-  grid[i].addEventListener('click', () => {
-    console.log(grid[i])
-  })
+function performClick() 
 
-  if (grid[i].onclick == grid[i]) {
-    console.log('grid 1 my guy!')
-  }
-}
+
+
+//   if (grid[i].onclick == grid[i]) {
+//     console.log('grid 1 my guy!')
+//   }
+// }
